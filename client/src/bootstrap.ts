42cb899bf50047afa995fe4549fb35e24c96c529
paiky <dev-miguel-mendez@proton.me>
2026-01-15 06:40:12 -0700

    update

--- client/src/bootstrap.ts
+++ client/src/bootstrap.ts
@@ -5,11 +5,22 @@
 const environment = process.env.ENVIRONMENT //$ This is set  in the npm scrips (like "npm run local")
 import dotenv from "dotenv"
 
+
+const requiredEnvVars: string[] = [
+    'NEXT_PUBLIC_BACKEND_HTTP_PROTOCOL',
+    'NEXT_PUBLIC_BACKEND_HOST',
+    'NEXT_PUBLIC_BACKEND_PORT',
+]
+
 const loadENV = ()=>{
     dotenv.config({path: `./config/${environment}.env`})
-    console.log(process.env.BACKEND_HTTP_PROTOCOL)
-    console.log(process.env.BACKEND_HOST)
-    console.log(process.env.BACKEND_PORT)
+
+    for (const env of requiredEnvVars) {
+        if(!process.env[env]){
+            throw new Error(`Environment variable ${env} is not set`)
+        }
+    }
+
 }
 
 
--- client/src/lib/urls.ts
+++ client/src/lib/urls.ts
@@ -1,11 +1,12 @@
 
 
-const protocol = process.env.BACKEND_HTTP_PROTOCOL
-const host = process.env.BACKEND_HOST
-const port = process.env.BACKEND_PORT
+const protocol = process.env.NEXT_PUBLIC_BACKEND_HTTP_PROTOCOL
+const host = process.env.NEXT_PUBLIC_BACKEND_HOST
+const port = process.env.NEXT_PUBLIC_BACKEND_PORT
+
 
 export const baseUrl = new URL(`${protocol}://${host}:${port}`)
 
 export const authCheckUrl =  new URL('/users/auth-check', baseUrl.href, ).toString()
 
-export const createUserUrl =  new URL('/users/create', baseUrl.href, ).toString()
+export const createUserUrl =  new URL('/users/create', baseUrl.href).toString()
