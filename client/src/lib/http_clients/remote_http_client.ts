607b6a5871d508692b61cd037f24bee8cf28b5bd
paiky <dev-miguel-mendez@proton.me>
2026-01-15 09:53:21 -0700

    update

--- client/src/lib/http_clients/remote_http_client.ts
+++ client/src/lib/http_clients/remote_http_client.ts
@@ -2,6 +2,8 @@
 
 import { createHttpClient } from "./create_http_client"
 
-export const remoteBackendUrl = process.env.NEXT_PUBLIC_REMOTE_BACKEND_URL!
 
+
+//$ Loading from it's own httpClient file prevents the client-side HttpClient to have access to a file where internal ENV variables are also loaded.
+export const remoteBackendUrl = process.env.NEXT_PUBLIC_REMOTE_BACKEND_URL!
 export const remoteAxiosClient = createHttpClient(remoteBackendUrl)
