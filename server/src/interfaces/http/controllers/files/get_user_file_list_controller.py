16e4b0f686c59da5ca3303aed289a45de14c37f8
paiky <dev-miguel-mendez@proton.me>
2026-01-16 10:30:44 -0700

    update

--- server/src/interfaces/http/controllers/files/get_user_file_list_controller.py
+++ server/src/interfaces/http/controllers/files/get_user_file_list_controller.py
@@ -2,17 +2,12 @@ from fastapi import Depends
 from fastapi.responses import JSONResponse
 from entities.user_entity import User
 from interfaces.http.dependencies.auth_user_provider import get_auth_user_provider
-from interfaces.http.exceptions.custom_exception_instances import UnprocessableEntity
-
 
 async def get_user_file_list_controller (
     authenticated_user: User = Depends(get_auth_user_provider),
 )-> JSONResponse:
 
     files_list = authenticated_user.files
-
-
-    raise UnprocessableEntity("lol")
 
 
     return JSONResponse(status_code=200, content=[file.to_dto() for file in files_list])
