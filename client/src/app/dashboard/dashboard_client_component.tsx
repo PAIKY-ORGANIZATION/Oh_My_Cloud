cd1cb938f80ce12fcbefe9ab21d124692d668b26
paiky <dev-miguel-mendez@proton.me>
2026-01-15 19:24:14 -0700

    update

--- client/src/app/dashboard/dashboard_client_component.tsx
+++ client/src/app/dashboard/dashboard_client_component.tsx
@@ -10,15 +10,21 @@ import { useState } from "react"
 export function DashboardClientComponent ({initialUserFiles}: {initialUserFiles: UserFile []}) {
 
     const [userFiles, setUserFiles] = useState<UserFile []>(initialUserFiles)
+
     return (
-        <div>
-            {userFiles.map((userFile) => (
-                <div key={userFile.id}>
-                    <h1>{userFile.original_file_name}</h1>
-                    <p>{userFile.original_file_size}</p>
-                    <p>{userFile.id}</p>
-                </div>
-            ))}
+        <div className="border border-green-500 p-2">
+            <div className="flex flex-col gap-4">
+                {userFiles.map((userFile) => (
+                    <div key={userFile.id}>
+                        <h1>{userFile.original_file_name}</h1>
+                        <p>{userFile.original_file_size}</p>
+                        <p>{userFile.id}</p>
+                    </div>
+                ))}
+            </div>
+
+            <label htmlFor="upload-file">  Upload File </label>
+            <input id="upload-file" type="file" className="bg-blue-500 text-white p-1 hover:bg-blue-600 cursor-pointer"/>
         </div>
     )
 
